b10ae31efa35c1f92e9c82eb1e12c7e792fcf0c2
elizab22 <ebidwell04@gmail.com>
2023-04-13 17:50:41 -0500

    frontend stuff and backend in same place

--- App.js
+++ App.js
@@ -3,17 +3,27 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './pages/login';
-import Landing from './pages/landing';
-import Garden from './pages/garden';
-import FirstPrompt from './pages/question-one';
-import Plants from './pages/plants';
+import Login from './Frontend/pages/authentication/login';
+import Landing from './Frontend/pages/landing';
+import Garden from './Frontend/pages/garden/garden';
+import FirstPrompt from './Frontend/pages/authentication/question-one';
+import NewGarden from './Frontend/pages/garden/newGarden';
+import Plants from './Frontend/pages/plants';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import Register from './Frontend/pages/authentication/register';
+import Dashboard from './Frontend/pages/dashboard';
+import { useEffect } from './src/api-calls'
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    init();
+  }, [])
+  
+  EStyleSheet.build({})
   return (
-    <NavigationContainer>
+<NavigationContainer>
       <Stack.Navigator
        screenOptions={{
         headerShown: false,
@@ -32,12 +42,24 @@ export default function App() {
           component={FirstPrompt}
         />
         <Stack.Screen
+          name="NewGarden"
+          component={NewGarden}
+        />
+        <Stack.Screen
           name="Garden"
           component={Garden}
         />
         <Stack.Screen
           name="Plants"
           component={Plants}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+        />
+        <Stack.Screen
+          name="Dashboard"
+          component={Dashboard}
         />
       </Stack.Navigator>
     </NavigationContainer>
